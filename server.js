122d8bf8371f64d14c652e5eeb4b4c99c6c4c3ca
Germans Gruseckis <germans.gruseckis@accenture.com>
2019-02-17 12:02:33 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import './utils/dotenv';
+import defaultErrorHandler from './middlewares/defaultErrorHandler';
+import bodyParser from 'body-parser';
+
+const app = express();
+const logger = require('./utils/logger')('server');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use;
+
+app.use(`/api/v${process.env.API_VERSION}`, (req, res) => {
+  res.status(200).send({ message: 'Quiz-game API' });
+});
+
+app.use('/uploads', express.static('uploads'));
+app.use(defaultErrorHandler);
+
+const host = process.env[`HOST_${process.platform.toUpperCase()}`];
+const port = process.env.PORT || process.env.HOST_PORT;
+
+app.listen(port, host, () => {
+  logger.log('info', `App is running at http://${host}:${port} in ${app.get('env')} mode.`);
+});
