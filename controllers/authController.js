c7fa5f1cd3a15ae80858ea6b593f7a513bbfbfaa
Student <Student@LVAGPLTP0847_BC.dir.svc.accenture.com>
2019-02-18 13:41:56 +0200

    REFERENCE: QGA-7    DESCRIPTION: Made authController with Login and Register functions,    also routes for login and register in /routes/auth and in server.

---
+++ controllers/authController.js
@@ -0,0 +1,60 @@
+import jwt from 'jsonwebtoken';
+import AppError from '../errors/AppError';
+import * as UserModel from '../models/UserModel';
+
+const logger = require('../utils/logger')('logController');
+
+
+const register = async (req, res, next) => {
+    logger.log('debug', 'register: %j', req.body);
+    const { username, email, hashedPassword, name, surname, dateOfBirth } = req.body;
+    try {
+        await UserModel.save({
+            username: username,
+            email: email,
+            rehashedPassword: hashedPassword, //???
+            name: name,
+            surname: surname,
+            dateOfBirth: dateOfBirth,
+            level: 'user', //???
+        });
+        logger.log('info', `Successfully registered: ${req.body.username}`);
+        res.status(200).send({ payload: { message: 'Successfully registered' } });
+    } catch (error) {
+        next(new AppError(error.message, 400));
+    }
+};
+
+
+
+const login = async (req, res, next) => {
+    logger.log('debug', 'logIn: %j', req.body);
+    try {
+        const user = await UserModel.getUserByName(req.body.username);
+        if (!user) {
+            logger.log('debug', 'Login failed');
+            throw new AppError('Wrong user credentials!', 400);
+        }
+        const isPasswordsEqual = await UserModel.comparePassword({
+            userPassword: req.body.rehashedPassword,
+            hashedPassword: user.hashedPassword,
+        });
+        if (isPasswordsEqual) {
+            const token = jwt.sign(
+                {
+                    data: { username: user.username },
+                },
+                process.env.JWT_SECRET,
+                { expiresIn: '6h' }, //???
+            );
+            logger.log('info', `Successfully loged in: ${user.username}`);
+            res.status(200).send({ payload: { message: 'Successfully loged in', token } });
+        } else {
+            throw new AppError('Wrong user credentials!', 400);
+        }
+    } catch (error) {
+        next(error instanceof AppError ? error : new AppError('Wrong user credentials!', 400));
+    }
+};
+
+export { register, login };
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import defaultErrorHandler from './middlewares/defaultErrorHandler';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import index from './routes/index';
+import authRouter from './routes/auth';
+import authenticate from './middlewares/authenticate'
+
 
 const app = express();
 const logger = require('./utils/logger')('server');
@@ -25,7 +28,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routing
-
+//from Kristina, use authenticate method to add authentication to your route.
+app.use(`/api/v${process.env.API_VERSION}/auth`, authRouter);
 
 
 app.use(`/api/v${process.env.API_VERSION}`, index);
