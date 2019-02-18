d2840dbf90e96be6dc84494a977fefcce83dadbf
Bekhzod96 <mr.begzodjon96@mail.ru>
2019-02-18 17:12:17 +0200

    REFERANCE:QGA-12, DESCRIPTION:import mongoose from mongoose;

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,14 +1,65 @@
-const getRecords = async (req, res) => {
-  res.status(200).send({ message: "get request to /records" });
-};
-const postRecords = async (req, res) => {
-  res.status(200).send({ message: "post request to /records" });
-};
-const patchRecords = async (req, res) => {
-  res.status(200).send({ message: "patch request to /records" });
-};
-const deleteRecords = async (req, res) => {
-  res.status(200).send({ message: "delete request to /records" });
+import AppError from "../errors/AppError";
+
+import {
+  save,
+  getRecordById,
+  updateById,
+  deleteRecordById,
+  getAllRecords
+} from "../models/recordsModel";
+
+const getAllRecord = async (req, res) => {
+  try {
+    const record = await getAllRecords(req.body.recordId);
+    res.status(200).send({ payload: record });
+  } catch (error) {
+    next(new AppError(error.message));
+  }
 };
 
-export { getRecords, postRecords, deleteRecords, patchRecords };
+const getRecordsById = async (req, res) => {
+  try {
+    const record = await getRecordById(req.body.recordId);
+    res.status(200).send({ payload: record });
+  } catch (error) {
+    next(new AppError(error.message));
+  }
+};
+
+const addRecords = async (req, res, next) => {
+  try {
+    const record = await save({
+      questionId: req.body.questionId,
+      answers: req.body.answersId
+    });
+    res.status(201).send({ message: { payload: record } });
+  } catch (error) {
+    next(new AppError(error.message, 400));
+  }
+};
+
+const patchRecords = async (req, res, next) => {
+  try {
+    const record = await updateById(req.params.recordId, { ...req.body });
+    res.status(200).send({ payload: record });
+  } catch (error) {
+    next(new AppError(error.message));
+  }
+};
+
+const deleteRecords = async (req, res) => {
+  try {
+    const remove = await deleteRecordById(req.body.recordId);
+    res.status(200).send({ payload: "Record has been deleted" });
+  } catch (error) {
+    next(new AppError(error.message));
+  }
+};
+
+export {
+  getRecordsById,
+  getAllRecord,
+  addRecords,
+  deleteRecords,
+  patchRecords
+};
--- routes/recordsRoute.js
+++ routes/recordsRoute.js
@@ -1,17 +1,18 @@
 import express from "express";
 
 import {
-  getRecords,
-  postRecords,
+  getRecordsById,
+  getAllRecord,
+  addRecords,
   deleteRecords,
   patchRecords
 } from "../controllers/recordsController";
 
 const router = express.Router();
 
-router.get("/*", getRecords);
-router.post("/*", postRecords);
-router.delete("/*", deleteRecords);
-router.patch("/*", patchRecords);
+router.get("/", getAllRecord);
+router.post("/", addRecords);
+router.delete("/", deleteRecords);
+router.patch("/", patchRecords);
 
 export default router;
