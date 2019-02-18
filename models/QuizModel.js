5b88c5a66129a94bcfadcf1659e69a837e3a6293
sadinovsr <Student@LVAGPLTP0877_BC.dir.svc.accenture.com>
2019-02-18 14:18:09 +0200

    REFERENCE: QGA-2   DESCRIPTION: Created QuizModel

---
+++ models/QuizModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const quizSchema = new mongoose.Schema(
+  {
+    ownerId: { type: String, unique: false, required: true },
+    name: { type: String, unique: true, required: true },
+    questions: [{ type: String, unique: false, required: false }]
+  },
+  { timestamps: true }
+);
+
+const QuizModel = mongoose.model('Quiz', quizSchema);
+
+const save = async model => new QuizModel(model).save();
+
+const getAllQuizzes = async () => QuizModel.find();
+
+const getQuizById = async _id => QuizModel.findOne({ _id });
+
+const getQuizzesByOwnerId = async ownerId => QuizModel.find({ ownerId });
+
+const updateQuizById = async (id, model) => QuizModel.findByIdAndUpdate(id, model);
+
+const deleteQuizById = async _id => QuizModel.deleteOne({ _id });
+
+export { save, getAllQuizzes, getQuizById, getQuizzesByOwnerId, updateQuizById, deleteQuizById };
