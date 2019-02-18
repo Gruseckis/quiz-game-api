e63b6db15ae8074b60ab02f9eb66c12c60a3e7f6
Pozelaite <jana.pozelaite@accenture.com>
2019-02-18 17:50:20 +0200

    REFFERENCE: QGA-1 DESCRIPTION: Created user model

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+{
+  username: {type: String, trim: true, unique: true, required: true},
+  email: {type: String, trim: true, required: true},
+  hashedPassword: {type: String, trim: true, required: true},
+  name: {type: String, trim: true, required: true},
+  surname: {type: String, trim: true, required: true},
+  dateOfBirth: {type: Date, trim: true, required: true},
+  level: {type: String, trim: true, required: true, enum :['user','quizer','moderator','admin']}
+},
+{
+  timestamp: true
+});
+
+userSchema.pre('save', async function callback(next){
+  if(this.hashedPassword){
+    this.hashedPassword = await bcrypt.hash(
+      this.hashedPassword,
+      parseInt(process.env.PASSWORD_HASHING_ROUNDS,10)
+    );
+  }
+  next();
+});
+
+const UserModel = mongoose.model('User',userSchema);
+
+const save = async model => new UserModel(model).save();
+const getUserByUsername = async username => UserModel.findOne({username});
+const getUsers = async () => UserModel.find();
+const updateUser = async(id,model) => UserModel.findByIdAndUpdate(id, model, {new: true});
+const deleteUser = async(id) =>  UserModel.findByIdAndRemove(id);
+const comparePassword = async ({userPassword, hashedPassword}) => bcrypt.compare(userPassword, hashedPassword);
+
+UserModel.schema
+  .path('username')
+  .validate(async username => !(await getUserByName(username)), 'User already exists!');
+
+export { save, getUserByUsername, comparePassword, userSchema, getUsers, UserModel, updateUser, deleteUser };
