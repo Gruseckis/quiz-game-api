86637c33040038ef9b5aabc1b486266420d3dc1d
Student <Student@LVAGPLTP0847_BC.dir.svc.accenture.com>
2019-02-18 16:26:33 +0200

    REFERENCE: QGA-7    DESCRIPTION: made controller and route for registration and login,    also added auth route to server.js.

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,17 +7,18 @@ const logger = require('../utils/logger')('logController');
 
 const register = async (req, res, next) => {
     logger.log('debug', 'register: %j', req.body);
-    const { username, email, hashedPassword, name, surname, dateOfBirth } = req.body;
+    const { username, email, hashedPassword, name, surname, dateOfBirth, level } = req.body;
     try {
         await UserModel.save({
             username: username,
             email: email,
-            rehashedPassword: hashedPassword, //???
+            hashedPassword: hashedPassword,
             name: name,
             surname: surname,
             dateOfBirth: dateOfBirth,
-            level: 'user', //???
+            level: level, //???
         });
+        console.log(req.body.level)
         logger.log('info', `Successfully registered: ${req.body.username}`);
         res.status(200).send({ payload: { message: 'Successfully registered' } });
     } catch (error) {
@@ -31,12 +32,15 @@ const login = async (req, res, next) => {
     logger.log('debug', 'logIn: %j', req.body);
     try {
         const user = await UserModel.getUserByName(req.body.username);
+        console.log(req.body.level)
+
         if (!user) {
             logger.log('debug', 'Login failed');
             throw new AppError('Wrong user credentials!', 400);
         }
+
         const isPasswordsEqual = await UserModel.comparePassword({
-            userPassword: req.body.rehashedPassword,
+            userPassword: req.body.hashedPassword,
             hashedPassword: user.hashedPassword,
         });
         if (isPasswordsEqual) {
@@ -50,7 +54,7 @@ const login = async (req, res, next) => {
             logger.log('info', `Successfully loged in: ${user.username}`);
             res.status(200).send({ payload: { message: 'Successfully loged in', token } });
         } else {
-            throw new AppError('Wrong user credentials!', 400);
+            throw new AppError('CHECK TOKEN PART', 400);
         }
     } catch (error) {
         next(error instanceof AppError ? error : new AppError('Wrong user credentials!', 400));
