69f2b2f4607e6dfecfc9ebf90113371d334a7806
Bekhzod96 <mr.begzodjon96@mail.ru>
2019-02-19 00:08:26 +0200

    REFERANCE: QGA-5 DESCRIPTION: Record Model has been created and modified and added  additional validation

--- models/recordsModel.js
+++ models/recordsModel.js
@@ -11,15 +11,14 @@ const RecordModel = mongoose.model("Record", recordSchema);
 
 const save = async model => new RecordModel(model).save();
 const getAllRecords = async () => RecordModel.find();
-const getRecordById = async recordId => RecordModel.findOne(recordId);
+const getRecordById = async _id => RecordModel.findById({ _id });
 
 const updateById = async (recordId, update) =>
-  RecordModel.findOneAndUpdate(recordId, update, { new: true });
+  RecordModel.findByIdAndUpdate(recordId, update, { new: true });
 
 const deleteRecordById = async recordId =>
-  RecordModel.findOneAndDelete(recordId);
+  RecordModel.findByIdAndRemove(recordId);
 
-// What acctual data we need to handle throuw
 const getRecordsFromIdArray = async recordId =>
   RefordModel.find({
     recordID: { $in: [mongoose.Types.ObjectId(recordId.map())] }
