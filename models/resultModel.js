f436790527d135515d801b5bcfc03835ce7754e2
Student <Student@LVAGPLTP0788_BC.dir.svc.accenture.com>
2019-02-19 12:32:00 +0200

    REFERENCE: QGA-4 (2) DESCRIPTION: Changes based on review.

--- models/resultModel.js
+++ models/resultModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const resultSchema = new mongoose.Schema({
 
-    recordId: { type: Array, unique: true, required: true },
+    recordId: { type: [String], unique: true, required: true },
     userId: { type: String, unique: true, required: true },
     quizId: { type: String, unique: true, required: true },
 },
@@ -17,17 +17,15 @@ const save = async model => new ResultModel(model).save();
 
 const getAllResults = async () => ResultModel.find();
 
-const getResultById = async _id => ResultModel.findById({ _id });
+const getResultById = async _id => ResultModel.findById(_id);
 
-const getAllResultsByUserId = async userId => ResultModel.find({ userId });
+const getAllResultsByUserId = async userId => ResultModel.find(userId);
 
-const getAllresultsByQuizId = async quizId => ResultModel.findOne({ quizId });
-
-const updateResultById = async _id => ResultModel.findById({ _id });
+const getAllresultsByQuizId = async quizId => ResultModel.findOne(quizId);
 
 const findByIdAndUpdate = async (id, model) => ResultModel.findByIdAndUpdate(id, model, { new: true });
 
 const deleteResultById = async _id => ResultModel.findByIdAndRemove(_id);
 
 
-export { save, getAllResults, getResultById, getAllResultsByUserId, getAllresultsByQuizId, updateResultById, deleteResultById, ResultModel, resultSchema, findByIdAndUpdate };
+export { save, getAllResults, getResultById, getAllResultsByUserId, getAllresultsByQuizId, deleteResultById, ResultModel, resultSchema, findByIdAndUpdate };
