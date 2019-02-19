21c97fd2a9953180c4e41fe6a7bc66fc3715616a
Bekhzod96 <mr.begzodjon96@mail.ru>
2019-02-19 17:36:29 +0200

    REFERANCE:QGA-12-Create-Record-Routes DESCRIPTION: FROM SERVER WAS DELETED SOME LINES

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import usersRoutes from './routes/usersRoutes';
 import quizRoutes from './routes/quizRouter';
 
 import authRouter from './routes/authRouter';
-import authenticate from './middlewares/authenticate'
+import authenticate from './middlewares/authenticate';
 
 const app = express();
 const logger = require('./utils/logger')('server');
@@ -32,7 +32,9 @@ mongoose.connection.once('open', () =>
   logger.log('info', 'MongoDB has been connected.')
 );
 
-mongoose.connection.once('open', () => logger.log('info', 'MongoDB has been connected.'));
+mongoose.connection.once('open', () =>
+  logger.log('info', 'MongoDB has been connected.')
+);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -40,14 +42,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(`/api/v${process.env.API_VERSION}/auth`, authRouter);
 app.use(`/api/v${process.env.API_VERSION}/records`, recordsRouter);
-app.use(`/api/v${process.env.API_VERSION}/quizzes`, quizRoutes);
 app.use(`/api/v${process.env.API_VERSION}`, index);
 app.use(`/api/v${process.env.API_VERSION}/questions`, questionRouter);
-app.use(`/api/v${process.env.API_VERSION}/auth`, authRouter);
-
 app.use(`/api/v${process.env.API_VERSION}/users`, authenticate, usersRoutes);
 app.use(`/api/v${process.env.API_VERSION}/quizzes`, authenticate, quizRoutes);
-
 
 app.use(`/api/v${process.env.API_VERSION}`, index);
 app.use('/uploads', express.static('uploads'));
