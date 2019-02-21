2871a79bae73d64e7d4c85469d37f2b11c33a5b9
Student <Student@LVAGPLTP0847_BC.dir.svc.accenture.com>
2019-02-21 16:38:27 +0200

    REFERENCE: QGA-50    DESCRIPTION: added userId to question Schema and made both userId    and answer to be unique, so one user can answer question only once

--- models/questionModel.js
+++ models/questionModel.js
@@ -3,12 +3,16 @@ import mongoose from 'mongoose';
 const questionSchema = new mongoose.Schema(
    {
       "question": { type: String, required: true },
-      "correct": [ {type: String, required: true } ], // array of correct answer ID's 
-      "answers": [ {type: String, required: true } ], // array of possible answers as strings
-      "type": { type: String, required: true, enum: [ 'input', 'checkout', 'radio', 'textbox' ]}
+      "correct": [{ type: String, required: true }], // array of correct answer ID's 
+      "answers": [{ type: String, required: true, unique: false }], // array of possible answers as strings
+      "type": { type: String, required: true, enum: ['input', 'checkout', 'radio', 'textbox'] },
+      "userId": { type: String, unique: false, required: true },
    },
    { timestamps: true },
 );
+
+questionSchema.index({ userId: 1, answers: 1 }, { unique: true });
+
 const QuestionModel = mongoose.model('Question', questionSchema);
 
 // save your question to DB
@@ -21,9 +25,9 @@ const getAllQuestions = async () => QuestionModel.find();
 const getQuestionByID = async _id => QuestionModel.findById(_id);
 
 // updates question by provided ID
-const updateQuestionByID = async (_id, update) => QuestionModel.findByIdAndUpdate(_id, update, {new: true });
+const updateQuestionByID = async (_id, update) => QuestionModel.findByIdAndUpdate(_id, update, { new: true });
 
 // delete question by provided ID(only quis owner or admin can delete questions)
 const deleteQuestionByID = async _id => QuestionModel.findByIdAndDelete(_id);
 
-export  { QuestionModel, questionSchema, save, getAllQuestions, getQuestionByID, updateQuestionByID, deleteQuestionByID };
+export { QuestionModel, questionSchema, save, getAllQuestions, getQuestionByID, updateQuestionByID, deleteQuestionByID };
